74f07546de9f7b1ad27f02c01f0a633a4b345f76
Suraj <hingadesuraj@gmail.com>
2024-02-15 11:48:23 +0530

    Day Start : Build Patym Frontend

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,7 @@ import "./App.css";
 
 function App() {
   return <>
-   {/* <div style={{display:'flex',padding:"10px",justifyContent:"center",alignItems:"center"}} >
-    <div style={{margin:'5px',border:"1px solid black",padding:"10px"}}>div One</div>
-    <div style={{margin:'5px',border:"1px solid black",padding:"10px"}}>div Two</div>
-    <div style={{margin:'5px',border:"1px solid black",padding:"10px"}} >div Three</div>
-   </div> */}
-   {/* <div> 
-   <h1 className="text-3xl text-blue-500 font-bold underline">
-      Hello world!
-    </h1>
-   </div> */}
-
-    <div className="grid grid-cols-1 md:grid-cols-3" >
-      <div className="bg-red-500">RED</div>
-      <div className="bg-yellow-500">Yellow</div>
-      <div className="bg-blue-500">Blue</div>
-    </div>
-
-    {/* <div className=" bg-blue-400 sm:bg-red-400 md:bg-yellow-300 lg:bg-orange-400 xl:bg-slate-500 2xl:bg-violet-600 " >
-      <h3>Hello suraj bhau </h3>
-    </div> */}
+    
 
   </>;
 }
