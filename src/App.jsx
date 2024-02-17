691dae27749b760e2f9a133a1752269f7c626759
Suraj <hingadesuraj@gmail.com>
2024-02-17 21:24:57 +0530

    Code to connect postgre db using pg npm package

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,50 @@
+import { useEffect } from "react";
 import "./App.css";
+import { Client } from "pg";
+
 
 function App() {
-  return <>
-    
-    
 
-  </>;
+  // const client = new Client({
+  //   host: 'URL',
+  //   port: 5334,
+  //   database: 'newDB',
+  //   user: 'user',
+  //   password: "password",
+  // })
+  
+  // client.connect();
+
+
+  const client = new Client({
+    connectionString: "postgreURL"
+  })
+  
+  async function createUsersTable() {
+      await client.connect()
+      const result = await client.query(`
+          CREATE TABLE users (
+              id SERIAL PRIMARY KEY,
+              username VARCHAR(50) UNIQUE NOT NULL,
+              email VARCHAR(255) UNIQUE NOT NULL,
+              password VARCHAR(255) NOT NULL,
+              created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+          );
+      `)
+      console.log(result)
+  }
+  useEffect(()=>{
+
+    createUsersTable();
+  },[])
+
+
+
+  return (
+    <>
+      <h1>Hello from react implement postgreSQL Database</h1>
+    </>
+  );
 }
 
 export default App;
